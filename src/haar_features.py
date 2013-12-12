Fixed feature calculation

src/haar_features.py
@@ -20,7 +20,7 @@ def generateHaarFeatures(number):
     prototype = np.array(
         [0, 0, 1, 1, 0.25, 0.25, 0.5, 0.5])
 
-    scale = random.uniform(0.1, 0.5, (number, 1))
+    scale = random.uniform(0.3, 0.8, (number, 1))
 
     protoypes = np.tile(prototype, (number, 1)) * scale
 
@@ -73,7 +73,7 @@ def calculateValues(rectangle, haar_features, indices=None):
             D = cv2.getRectSubPix(rectangle, (1, 1), (x2, y2))
             A = cv2.getRectSubPix(rectangle, (1, 1), (x1, y1))
             B = cv2.getRectSubPix(rectangle, (1, 1), (x1, y2))
-            C = cv2.getRectSubPix(rectangle, (1, 1), (y2, x1))
+            C = cv2.getRectSubPix(rectangle, (1, 1), (x2, x1))
             areaOuter = D + A - B - C
             # print x1,y1,x2,y2
             # print D,A,B,C
@@ -84,7 +84,7 @@ def calculateValues(rectangle, haar_features, indices=None):
             D = cv2.getRectSubPix(rectangle, (1, 1), (x2, y2))
             A = cv2.getRectSubPix(rectangle, (1, 1), (x1, y1))
             B = cv2.getRectSubPix(rectangle, (1, 1), (x1, y2))
-            C = cv2.getRectSubPix(rectangle, (1, 1), (y2, x1))
+            C = cv2.getRectSubPix(rectangle, (1, 1), (x2, x1))
             areaInner = D + A - B - C
 
             c = i % 3
@@ -99,7 +99,7 @@ def calculateValues(rectangle, haar_features, indices=None):
             D = cv2.getRectSubPix(rectangle, (1, 1), (x2, y2))
             A = cv2.getRectSubPix(rectangle, (1, 1), (x1, y1))
             B = cv2.getRectSubPix(rectangle, (1, 1), (x1, y2))
-            C = cv2.getRectSubPix(rectangle, (1, 1), (y2, x1))
+            C = cv2.getRectSubPix(rectangle, (1, 1), (x2, x1))
             areaOuter = D + A - B - C
             # print x1,y1,x2,y2
             # print D,A,B,C
@@ -110,7 +110,7 @@ def calculateValues(rectangle, haar_features, indices=None):
             D = cv2.getRectSubPix(rectangle, (1, 1), (x2, y2))
             A = cv2.getRectSubPix(rectangle, (1, 1), (x1, y1))
             B = cv2.getRectSubPix(rectangle, (1, 1), (x1, y2))
-            C = cv2.getRectSubPix(rectangle, (1, 1), (y2, x1))
+            C = cv2.getRectSubPix(rectangle, (1, 1), (x2, x1))
             areaInner = D + A - B - C
             values[i:i+3] = (areaOuter - 2 * areaInner).ravel()
             # print x1,y1,x2,y2