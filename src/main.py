f4a4586942001ea02813b871448c1950bdc46ed0
snuderl <snuderl>
2013-12-15 16:57:42 -0800

    wtf

--- src/main.py
+++ src/main.py
@@ -21,8 +21,8 @@ ext = ".avi"
 filename = "Vid_A_ball"
 target = (200, 110, 50, 55)
 
-filename = "Vid_B_cup"
-target = (0.38960 * 320, 0.384615 * 240, 0.146011 * 320, 0.2440651 * 240)
+#filename = "Vid_B_cup"
+#target = (0.38960 * 320, 0.384615 * 240, 0.146011 * 320, 0.2440651 * 240)
 
 POS_EXAMPLES = 15
 NEG_EXAMPLES = 140
@@ -41,8 +41,8 @@ PARTICLES = 2000
 #filename = "Vid_E_person_partially_occluded"
 #target = (0.434343*320, 0.18461*240, 0.167388*320, 0.675*240)
 
-filename = "Vid_C_juice"
-target = (0 * 320, 0.410029 * 240, 0.208388 * 320, 0.114061 * 240)
+#filename = "Vid_C_juice"
+#target = (0 * 320, 0.410029 * 240, 0.208388 * 320, 0.114061 * 240)
 
 if camera:
     capture = cv2.VideoCapture(0)
